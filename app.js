f890320627d5445982f6462525e0445cf420b805
aldilla afiani alda <aldilla90@gmail.com>
2024-10-10 23:07:50 +0900

    first commit

---
+++ app.js
@@ -0,0 +1,22 @@
+const express   = require('express')
+const app       = express()
+const port      = 3000
+const c_beranda = require('./controller/c_beranda')
+const c_auth    = require('./controller/c_auth')
+
+
+app.use( express.urlencoded({extended:false}) )
+app.use( express.static('public') )
+
+
+app.set('view engine', 'ejs')
+app.set('views', './view')
+
+
+app.get('/', c_beranda.halaman_awal)
+app.get('/auth/login', c_auth.halaman_login)
+
+
+app.listen(port, ()=>{
+    console.log(`Aplikasi sudah siap, buka http://localhost:${port}`)
+})
---
+++ controller/c_auth.js
@@ -0,0 +1,8 @@
+module.exports =
+{
+
+    halaman_login: function(req,res) {
+        res.render('v_auth/login')
+    }
+
+}
